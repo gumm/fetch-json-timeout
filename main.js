ce5c59c1f62ae1ec0663220e86bc4b3f680f894d
Jan Badenhorst <jan@trintel.co.za>
2022-02-24 13:32:50 +0200

    Non-backwards compatible changes. Implements JWT auth and refresh.

--- main.js
+++ main.js
@@ -3,28 +3,111 @@
  */
 import base64 from 'base-64';
 import fetch from 'node-fetch';
+import timeoutSignal from 'timeout-signal';
 
-/**
- * @param {!Object} cred
- * @return {!Function}
- */
-const jsonInit = ([uName, pWord]) => {
-  const h = new fetch.Headers();
-  h.append('Content-type', 'application/json');
-  h.append('X-Requested-With', 'XMLHttpRequest');
-  if (uName && pWord) {
-    h.append('Authorization', 'Basic ' + base64.encode(`${uName}:${pWord}`));
-  }
+
+const parseJwt = token => {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(Buffer.from(base64, 'base64').toString().split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload).exp;
+};
+
+
+const refreshJWTToken = async (jwtObj, currentToken) => {
+
+  const url = jwtObj.refreshUri.toString();
   const rep = {
     cache: 'no-cache',
-    headers: h,
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: jwtObj.verb,
+    body: JSON.stringify({
+      "token":currentToken
+    })
   };
+
+  return await fetch(url, rep)
+      .then(checkStatus)
+      .then(getJson)
+      .catch(e => console.log('Token Refresh Error', e));
+}
+
+const getJWTToken = async jwtObj => {
+
+  const url = jwtObj.uri.toString();
+  const rep = {
+    cache: 'no-cache',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    method: jwtObj.verb,
+    body: JSON.stringify(jwtObj.payload)
+  };
+
+  return await fetch(url, rep)
+      .then(checkStatus)
+      .then(getJson)
+      .catch(e => console.log('Token Fetch Error', e));
+}
+
+/**
+ * @param uName
+ * @param pWord
+ * @param jwtObj
+ * @returns {Promise<function(*=, *=, *=): Promise<{method: string, signal: AbortSignal} & {cache: string}>>}
+ */
+const jsonInit = async ([uName, pWord], jwtObj) => {
+  const h = {
+    'Content-type': 'application/json'
+  };
+  const rep = {
+    cache: 'no-cache'
+  };
+  let tokenExpr = 0;
+  let JWTToken = "";
+
   if (uName && pWord) {
-    rep['credentials'] = 'include'
+    h.Authorization = 'Basic ' + base64.encode(`${uName}:${pWord}`);
+    rep.credentials = 'include'
   }
 
-  return (verb = 'GET', obj = undefined) => {
-    const r = Object.assign({method: verb}, rep);
+  const updateTokenInfo = tpl => {
+    if (tpl) {
+      JWTToken = tpl.token;
+      h.Authorization = 'Bearer ' + JWTToken;
+      rep.credentials = 'include';
+      tokenExpr = parseJwt(JWTToken);
+    }
+  }
+
+  const checkToken = async () => {
+    if (jwtObj && tokenExpr > 0) {
+      const now =  Math.floor(Date.now() / 1000);
+      if (tokenExpr - now <= 10) {
+        const tokenPayload = await refreshJWTToken(jwtObj, JWTToken);
+        updateTokenInfo(tokenPayload);
+      }
+    }
+  }
+
+  if (jwtObj) {
+    const tokenPayload = await getJWTToken(jwtObj);
+    updateTokenInfo(tokenPayload);
+  }
+
+  return async (to = 60000, verb = 'GET', obj = undefined) => {
+
+    await checkToken();
+
+    rep.headers = h;
+    const r = Object.assign({
+      method: verb,
+      signal: timeoutSignal(to)
+    }, rep);
     if (obj) {
       r.body = JSON.stringify(obj)
     }
@@ -79,43 +162,32 @@ const callbackAndData = cb => data => {
 
 
 /**
- * @param {!string} uName
- * @param {!string} pWord
- * @returns {function(string, string, number= Function=, Object=): Promise<T>}
+ * @param {!string=} uName
+ * @param {!string=} pWord
+ * @param {Object=} jwtObj
+ * @returns {Promise<function(string, string, number=, function(*=): *=, Object=): Promise<unknown | void>>}
  */
-export default (uName = undefined, pWord = undefined) => {
-  const init = jsonInit([uName, pWord]);
+export default async (uName = undefined, pWord = undefined, jwtObj = undefined) => {
+  let init = await jsonInit([uName, pWord], jwtObj);
 
   /**
    * @param {string} verb GET, POST, PUT etc.
    * @param {string} uri
    * @param {number=} to Timeout in ms. Defaults to 60 seconds
    * @param {function(*=):*} cb
-   * @param {Object=} opt_pl
+   * @param {Object=} opt_pl Post payload. JSON converted to body property
    * @returns {Promise<T>}
    */
-  const func = (verb, uri, to = 60000, cb = () => null, opt_pl = undefined) => {
-    const req = new fetch.Request(uri.toString());
+  const func = async (verb, uri, to = 60000, cb = () => null, opt_pl = undefined) => {
+    const req = uri.toString();
+    const options = await init(to, verb, opt_pl);
 
-    let cTmOut = a => a;
-    const timeout = () => new Promise((resolve, reject) => {
-      const tid = setTimeout(reject, to, 'Fetch Timeout');
-      cTmOut = () => clearTimeout(tid);
-    });
-
-    const f = fetch(req, init(verb, opt_pl))
-        .then(d => {
-          cTmOut();
-          return d;
-        })
+    return fetch(req, options)
         .then(checkStatus)
         .then(getJson)
         .then(callbackAndData(cb))
         .catch(e => console.log('Data Error', e));
 
-    return Promise.race([f, timeout()]).catch(err => {
-      console.log('JSON Fetch Errors:', err)
-    });
   };
 
   return func;
--- test/basic_tests.js
+++ test/basic_tests.js
@@ -1,21 +1,73 @@
 import assert from 'assert';
 import fetchJson from '../main.js';
+import {promises as fs} from "fs";
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// README:
+// -----------------------------------------------------------------------------
+// Replace JWTObj.json with an equivalent object with your
+// JWT credentials and info.
+// For ease of testing, you can add two fields to this object that defines the
+// test URLs to use for JWT token testing, and the expected response from that
+// endpoint.
+//  "testUrl": "https://some.test.endpoint/api/get_data/",
+//  "expectedResponse": { "here": [ "is", "your", "response" ] }
+// -----------------------------------------------------------------------------
+
+
+const jwtPrefFilePath = new URL('./JWTObj.json', import.meta.url);
+const getJWTPrefObj = async () => {
+  return fs.readFile(jwtPrefFilePath, 'binary')
+      .then(JSON.parse)
+      .catch(e => {
+        console.error('ERROR reading JWT pref obj: ', e);
+        return {}
+      });
+};
+
+
+describe('Init a fetcher with token payload', () => {
+
+  it('promise resolves with payload', async () => {
+    const jwtOpts = await getJWTPrefObj();
+    const url = jwtOpts.testUrl;
+    const expected = jwtOpts.expectedResponse;
+
+    const fetcher = await fetchJson(undefined, undefined, jwtOpts);
+    const data = await fetcher('GET', url);
+    assert.deepEqual(data, expected);
+  });
+
+  it('callback resolves with payload', async () => {
+    const jwtOpts = await getJWTPrefObj();
+    const url = jwtOpts.testUrl;
+    const expected = jwtOpts.expectedResponse;
+
+    const fetcher = await fetchJson(undefined, undefined, jwtOpts);
+    fetcher('GET', url, undefined, data => {
+      console.log("Here us your data -> ", data);
+      assert.deepEqual(data, expected);
+    });
+  });
+});
+
 describe('Init a fetcher without credentials', () => {
-  const fetcher = fetchJson();
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-  it('promise resolves with payload', () => {
-    fetcher('GET', url).then(r => console.log(r));
+  it('promise resolves with payload', async () => {
+    const fetcher = await fetchJson();
+    const data = await fetcher('GET', url);
+    console.log(data);
   });
 
-  it('callback resolves with payload', () => {
-    fetcher('GET', url, undefined, r => console.log(r));
+  it('callback resolves with payload', async () => {
+    const fetcher = await fetchJson();
+    fetcher('GET', url, undefined, console.log);
   });
 
-  it('failure is an option', () => {
-    fetcher('GET', 'https://this.wont.resolve', 5).then(e => console.log(e));
+  it('failure is an option', async () => {
+    const fetcher = await fetchJson();
+    fetcher('GET', 'https://this.wont.resolve', 5000).then(console.log);
   });
-
 
 });
+
